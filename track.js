d8a30032616908fb5d634564630eb8fbaa17abd2
Christian Tellnes <christian@tellnes.no>
2012-05-27 08:36:52 +0200

    initial commit

---
+++ track.js
@@ -0,0 +1,83 @@
+(function() {
+
+  nextTick = process && process.nextTick || function(fn) {
+    setTimeout(fn, 0)
+  }
+
+  function track(end) {
+    var results = []
+      , firstTick = true
+      , length = 0
+      , called = 0
+
+    nextTick(function() {
+      firstTick = false
+    })
+
+    function check() {
+      if (end && length == called) {
+        end(null, results)
+      }
+    }
+
+    function self(fn) {
+      var id = length++
+        , cb
+
+      if (fn) {
+        cb = self()
+      }
+
+      return function callback(err) {
+        var args = Array.prototype.slice.call(arguments)
+
+        if (firstTick) {
+          args.unshift(this)
+          nextTick(callback.bind.apply(callback, args))
+          return
+        }
+
+
+        called++
+        results[id] = args.slice(1)
+
+
+        if (fn) {
+          var l = fn.length - 1
+
+          while (l > args.length) {
+            args.push(null)
+          }
+
+          args.push(cb)
+
+          fn.apply(this, args)
+
+        } else if (err) {
+          end && end(err)
+          end = null
+
+        } else {
+          check()
+        }
+      }
+    }
+
+
+    self.end = function(fn) {
+      if (fn) end = fn
+
+      if (!firstTick) check()
+    }
+  
+
+    return self
+  }
+
+  if (typeof module != 'undefined' && module.exports) {
+    module.exports = track
+  } else {
+    this.track = track
+  }
+
+}())
